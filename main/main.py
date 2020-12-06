95a91156e5720df6722634f9d602e897706db51a
JoseIMarti <jimartigil@gmail.com>
2020-12-06 12:00:47 +0100

    JIMA - Interface first launch

--- main/main.py
+++ main/main.py
@@ -3,3 +3,10 @@ Created on Dec 6, 2020
 
 @author: jima
 '''
+import sys
+from helpers.forms import *
+from PyQt5.QtWidgets import QApplication
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    sys.exit(app.exec_())
